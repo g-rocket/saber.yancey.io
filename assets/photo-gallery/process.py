f14c62f94e9e64d6e3606e11b2ca5e549d31573f
Gavin Yancey <g.rocket1@gmail.com>
2026-04-25 22:29:23 -0700

    add more photos

--- assets/photo-gallery/process.py
+++ assets/photo-gallery/process.py
@@ -1,4 +1,5 @@
-import os, subprocess, io
+#!/usr/bin/env python3
+import os, subprocess, io, sys
 
 def get_size(dim, maxw, maxh):
     if dim[0]/dim[1] < maxw/maxh:
@@ -21,9 +22,10 @@ with io.StringIO() as s:
     for f in reversed(sorted(files)):
         full = os.path.join(basedir, f)
         thumb = os.path.join(basedir, 'thumbs', f)
-        subprocess.run(['ffmpeg', '-y', '-i', full, '-vf', 'scale=800:600:force_original_aspect_ratio=decrease', thumb], check=True)
-        #w,h = get_size(get_dim(f), 800,600)
-        w,h = get_dim(thumb)
-        s.write(f'        <a href="{full}"><img width="{round(w)}" height="{round(h)}" src="{thumb}" /></a>\n')
+        if not ('--new' in sys.argv and os.path.exists(thumb)):
+            subprocess.run(['ffmpeg', '-y', '-i', full, '-vf', 'scale=800:600:force_original_aspect_ratio=decrease', thumb], check=True)
+            #w,h = get_size(get_dim(f), 800,600)
+            w,h = get_dim(thumb)
+            s.write(f'        <a href="{full}"><img width="{round(w)}" height="{round(h)}" src="{thumb}" /></a>\n')
     s.seek(0)
     print(s.read(), end='')
